6077e515cd9b2a7f03190d3d70ff3a41ba6eee02
jpr371 <ribasjoaopedro094@gmail.com>
2026-04-02 15:23:01 -0300

    efg

--- Program.cs
+++ Program.cs
@@ -2,18 +2,20 @@
 
 class Program {
     static void Main() {
-        Console.Write("Digite a quantidade de números (n): ");
-        int n = int.Parse(Console.ReadLine());
-        int[] vetor = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            Console.Write($"Digite o {i + 1}º número: ");
-            vetor[i] = int.Parse(Console.ReadLine());
-        }
+Console.Write("Quantidade n: ");
+int n = int.Parse(Console.ReadLine());
+int[] v = new int[n];
 
-        Console.WriteLine("\nNúmeros em ordem inversa:");
-        for (int i = n - 1; i >= 0; i--) {
-            Console.Write(vetor[i] + " ");
-        }
+for (int i = 0; i < n; i++) v[i] = int.Parse(Console.ReadLine());
+
+int maior = v[0], pos = 0;
+for (int i = 1; i < n; i++) {
+    if (v[i] > maior) {
+        maior = v[i];
+        pos = i;
+    }
+}
+Console.WriteLine($"Maior: {maior} na posição {pos}");
     }
 }
---
+++ exercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class exercicio1 {
+    static void Main() {
+        Console.Write("Digite a quantidade de números (n): ");
+        int n = int.Parse(Console.ReadLine());
+        int[] vetor = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            Console.Write($"Digite o {i + 1}º número: ");
+            vetor[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nNúmeros em ordem inversa:");
+        for (int i = n - 1; i >= 0; i--) {
+            Console.Write(vetor[i] + " ");
+        }
+    }
+}
